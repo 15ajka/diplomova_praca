85934276c1f4d7896d9eabd9190f4e9b47bfdc80
Angelika Pirhalova <angelikafedakova@gmail.com>
2021-11-27 19:09:01 +0100

    add example rendered png

--- rendering_sources/save_rgb.py
+++ rendering_sources/save_rgb.py
@@ -6,11 +6,12 @@ from matplotlib import pyplot as plt
 from mujoco_py import load_model_from_path, MjSim, functions
 import numpy as np
 
-model = load_model_from_path("../xmls/box.xml")
+model = load_model_from_path("box.xml")
 sim = MjSim(model)
 
 img = sim.render(width=500, height=500, camera_name=sys.argv[1])
 print(img)
 plt.imshow(img, interpolation='nearest')
+plt.savefig('upper_view.png')
 plt.show()
 
