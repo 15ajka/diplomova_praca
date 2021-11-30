64a39e14c1ccc58ac5da501f9df1c97bbc48f2e4
Angelika Pirhalova <angelikafedakova@gmail.com>
2021-11-30 20:08:43 +0100

    add basic textures

--- rendering_sources/create_xml.py
+++ rendering_sources/create_xml.py
@@ -4,16 +4,26 @@ import sys
 
 xml_prefix = """\
 <mujoco>
+   <asset>
+      <texture file="../checkerboard.png" type="cube" name="checker" />
+      <material name="checker-cube" reflectance="0.9" texrepeat="10.5 10.5" texture="checker" texuniform="true" />
+      <texture file="../sphere.png" type="2d" name="checker-sph" />
+      <material name="checker-sphere" reflectance="0.9" texrepeat="15 15" texture="checker-sph" texuniform="true" />
+   </asset>
    <worldbody>
       <light diffuse=".5 .5 .5" pos="0 0 3" dir="0 0 -1"/>
       <geom type="plane" size="1 1 0.1" rgba=".9 1 0 1"/>
      <body pos="0.5 0 0.1">
          <joint type="free"/>
-         <geom type="box" size=".1 .1 .1" rgba="0 .9 1 1"/>
+         <geom type="box" size=".1 .1 .1" material="checker-cube" />
       </body>
-      <body pos="0 0 0.1">
+      <body pos="0.5 0.5 0.1">
          <joint type="free"/>
-         <geom type="box" size=".1 .1 .1" rgba="0 .1 0.1 1"/>
+         <geom type="box" size=".1 .2 .1" material="checker-cube" rgba="1 .1 0.1 1"/>
+      </body>
+    <body pos="1 0 0.1">
+         <joint type="free"/>
+         <geom type="sphere" size=".1 .1 .1" material="checker-sphere" />
       </body>
 """
 
@@ -25,7 +35,7 @@ xml_suffix = """\
 
 camera_name = sys.argv[1]
 camera_line = f"""\
-        <camera axisangle="0 1 0 180" fovy="45" name="{camera_name}" pos="0 0 2"></camera>
+        <camera axisangle="0 1 0 0" fovy="45" name="{camera_name}" pos="0 0 2"></camera>
 """
 
 xml_string = xml_prefix + camera_line + xml_suffix
